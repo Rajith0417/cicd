18eba3cae67e33caf1fb45342266a489e035d47d
Raji G <rajith0417@gmail.com>
2025-06-14 19:57:06 +0530

    feat: test added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'cicd' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cicd');
+    expect(app.title).toEqual('cicd2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cicd');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cicd2');
   });
 });
